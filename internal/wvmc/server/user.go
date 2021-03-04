1323cbe896c918683ba66b5b723a9a11a4f95a81
Anaxita <anaxita.me@gmail.com>
2021-03-05 00:50:05 +0300

    add CreateUser handler

--- internal/wvmc/server/user.go
+++ internal/wvmc/server/user.go
@@ -2,9 +2,8 @@ package server
 
 import (
 	"database/sql"
+	"encoding/json"
 	"net/http"
-
-	// "sync"
 
 	"github.com/anaxita/wvmc/internal/wvmc/model"
 )
@@ -25,5 +24,26 @@ func (s *Server) GetUsers() http.HandlerFunc {
 			SendErr(w, http.StatusInternalServerError, err, "Ошибка БД")
 		}
 		SendOK(w, http.StatusOK, response{users})
+	}
+}
+
+// CreateUser создает пользователя
+func (s *Server) CreateUser() http.HandlerFunc {
+	type response struct {
+		UserID int `json:"id"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		req := model.User{}
+
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			SendErr(w, http.StatusBadRequest, err, "Неверный данные в запросе")
+		}
+
+		createdID, err := s.store.User(r.Context()).Create(req)
+		if err != nil {
+			SendErr(w, http.StatusInternalServerError, err, "Ошибка БД")
+		}
+		SendOK(w, http.StatusCreated, response{createdID})
 	}
 }
