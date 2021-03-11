f19eb02394ac589db7d1e9eee500f8aa3b0421a7
Anaxita <anaxita.me@gmail.com>
2021-03-12 00:55:59 +0300

    add commands to server service

--- internal/wvmc/control/commands.go
+++ internal/wvmc/control/commands.go
@@ -3,6 +3,7 @@ package control
 import (
 	"fmt"
 	"os/exec"
+	"strings"
 )
 
 // VM описывает свойства виртуальной машины, который можно получить с помощью комманд данного пакета
@@ -16,51 +17,73 @@ type VM struct {
 
 // Commander описывает метод который запускает команду powershell,возвращает вывод и ошибку
 type Commander interface {
-	Run(string) ([]byte, error)
+	run(string) ([]byte, error)
 }
 
 // Command содержит методы Run для запуска powershell команд
 type Command struct{}
 
-// ServerService содержит структуру, которая реализует интерфейс Commander
-type ServerService struct {
-	Commander Commander
-}
-
-// Run запускает команду powershell,возвращает вывод и ошибку
-func (c *Command) Run(command string) ([]byte, error) {
+// run запускает команду powershell,возвращает вывод и ошибку
+func (c *Command) run(command string) ([]byte, error) {
 	e := exec.Command("powershell", command)
 	out, _ := e.Output()
 	return out, nil
 }
 
+// ServerService содержит структуру, которая реализует интерфейс Commander
+type ServerService struct {
+	commander Commander
+}
+
 // NewServerService ...
 func NewServerService(c Commander) *ServerService {
 	return &ServerService{
-		Commander: c,
+		commander: c,
 	}
 }
 
-// GetServerStatus получает статус работы и сети ВМ
-func (s *ServerService) GetServerStatus(serverID string) ([]byte, error) {
-	command := fmt.Sprintf("Get-Server-Status -Name %s", serverID)
-	return s.Commander.Run(command)
+// GetServerStatus получает статус работы и сети ВМ servers по их ID
+func (s *ServerService) GetServerStatus(servers []string) ([]byte, error) {
+	script := `
+$result = New-Object System.Collections.Arraylist;
+foreach ($s in $servers) {
+	$power_status = $s.State
+	if ($power_status -eq 3) {
+		$network_status = 3
+	} else {
+		$network_status = ($s | Get-VMNetworkAdapter).Status
+	}
+
+	$vm = @{
+		"id" = $s.Id
+		"name" = $s.Name
+		"power" = $power_status
+		"network" = $network_status[0]
+	}
+
+	$result.Add($vm) | Out-Null;
+}
+$result | ConvertTo-Json;
+	`
+	command := fmt.Sprintf("$servers = Get-VM -ID %s ; %s", strings.Join(servers, ","), script)
+
+	return s.commander.run(command)
 }
 
 // StopServer выключает сервер
 func (s *ServerService) StopServer(serverID string) ([]byte, error) {
-	command := fmt.Sprintf("Get-Server-Status -Name %s", serverID)
-	return s.Commander.Run(command)
+	command := fmt.Sprintf("Stop-VM -ID %s", serverID)
+	return s.commander.run(command)
 }
 
 // StopServerForce принудительно выключает сервер
 func (s *ServerService) StopServerForce(serverID string) ([]byte, error) {
-	command := fmt.Sprintf("Get-Server-Status -Name %s", serverID)
-	return s.Commander.Run(command)
+	command := fmt.Sprintf("Stop-VM -ID %s -Force", serverID)
+	return s.commander.run(command)
 }
 
 // StartServer включает сервер
 func (s *ServerService) StartServer(serverID string) ([]byte, error) {
-	command := fmt.Sprintf("Get-Server-Status -Name %s", serverID)
-	return s.Commander.Run(command)
+	command := fmt.Sprintf("Start-VM -ID %s", serverID)
+	return s.commander.run(command)
 }
