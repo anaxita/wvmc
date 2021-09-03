3c072636af4b3c9a51f722afbc1479f698112825
anaxita <anaxita.me@gmail.com>
2021-09-03 22:23:19 +0300

    remove ip zinchenko

--- powershell/dev_GetVmForAdmins.ps1
+++ powershell/dev_GetVmForAdmins.ps1
@@ -4,7 +4,7 @@ param (
 
 [Console]::OutputEncoding = [System.Text.Encoding]::GetEncoding("utf-8")
 
-$nameList = 'ServerTwo' , 'VMBitrix_dev.kmsys.ru_off', 'VM_TestCentOS_off', 'SRV_IPZinchenko_off';
+$nameList = 'ServerTwo' , 'VMBitrix_dev.kmsys.ru_off', 'VM_TestCentOS_off';
 $hvList =  'DCSRVHV2', 'DCSRVHV7' ;
 
 $servers =  $hvList | ForEach-Object -Parallel {
