f3f2182a4915ed97d82a00f11788e4b5023d5620
anaxita <anaxita.me@gmail.com>
2021-09-06 02:19:08 +0300

    fix migrations

--- sql/refresh_tokens.sql
+++ sql/refresh_tokens.sql
@@ -1,10 +1,3 @@
--- CREATE TABLE IF NOT EXISTS `refresh_tokens` (
---   `user_id` int PRIMARY KEY,
---   `token` text NOT NULL,
---   UNIQUE KEY `user_id` (`user_id`)
--- );
-
-
 CREATE TABLE IF NOT EXISTS `refresh_tokens` (
   `user_id` int PRIMARY KEY,
   `token` text NOT NULL
