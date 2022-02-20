af479581f6ef1f56b79a3f0f93d8729868212ab2
Alexandr Kostiuchenko <anaxita.me@gmail.com>
2022-02-20 22:39:47 +0300

    added unique constrain title hv to servers

--- sql/servers.sql
+++ sql/servers.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `servers` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
   `vmid` varchar(255) NOT NULL DEFAULT "",
-  `title` varchar(255) NOT NULL DEFAULT "" UNIQUE ,
+  `title` varchar(255) NOT NULL DEFAULT "",
   `ip4` varchar(255) NOT NULL DEFAULT "",
   `hv` varchar(255) NOT NULL,
   `out_addr` varchar(255) NOT NULL DEFAULT "",
@@ -9,5 +9,6 @@ CREATE TABLE IF NOT EXISTS `servers` (
   `description` varchar(255) NOT NULL DEFAULT "",
   `company` varchar(255) NOT NULL DEFAULT "",
   `user_name` varchar(255) NOT NULL DEFAULT "",
-  `user_password` varchar(255) NOT NULL DEFAULT ""
+  `user_password` varchar(255) NOT NULL DEFAULT "",
+  UNIQUE (title, hv)
 );
